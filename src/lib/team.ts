2d8352c31491ecd5eb7c57420b9295b1c1c3df78
Sapper Dev <adam.weber@abstraktmg.com>
2026-06-04 12:38:37 +0000

    feat(team): add Alexandra Guarino LinkedIn (all 6 profiles now linked)

--- src/lib/team.ts
+++ src/lib/team.ts
@@ -18,7 +18,7 @@ export type TeamMember = {
 export const TEAM: TeamMember[] = [
   { name: "Amie Milner", title: "General Manager", initials: "AM", featured: true, photo: "/images/team/amie-milner.webp", linkedin: "https://www.linkedin.com/in/amiemilner/" },
   { name: "Tony Auck", title: "Vice President of Executive Sales", initials: "TA", photo: "/images/team/tony-auck.webp", linkedin: "https://www.linkedin.com/in/tony-auck-mba-43965b90/" },
-  { name: "Alexandra Guarino", title: "Director of Partner Success", initials: "AG", photo: "/images/team/alex-guarino.png" },
+  { name: "Alexandra Guarino", title: "Director of Partner Success", initials: "AG", photo: "/images/team/alex-guarino.png", linkedin: "https://www.linkedin.com/in/alexandraguarino5/" },
   { name: "Doug Jennings", title: "Director of Operations", initials: "DJ", photo: "/images/team/doug-jennings.png", linkedin: "https://www.linkedin.com/in/douglasjennings314/" },
   { name: "Natalie Archer", title: "Senior Sales Representative", initials: "NA", photo: "/images/team/natalie-archer.png", linkedin: "https://www.linkedin.com/in/nataliechristinearcher/" },
   { name: "Jeff Winters", title: "Founder", initials: "JW", photo: "/images/team/jeff-winters.webp", linkedin: "https://www.linkedin.com/in/jeffreyscottwinters/" },
